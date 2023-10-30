9c411c064d256f6fb49244e129b6d05eaa07eed9
Will Gillette <wgillette02@gmail.com>
2023-10-30 09:38:12 -0400

    Bug fixes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Divider from '@mui/material/Divider';
 import { Typography } from '@mui/material';
 import Biography from '../text/bio.js';
 import RootLayout from '../components/Layout';
+const headshot = 'https://media.licdn.com/dms/image/D4D03AQF_Sbojz5642w/profile-displayphoto-shrink_400_400/0/1697126489786?e=1704326400&v=beta&t=aN6qvPthhSKZv7eOZw1ODxUyF2Maj5eUU515xY0UZ9E';
 
 export default function Main() {
   return (
@@ -12,7 +13,7 @@ export default function Main() {
       <div className={styles.Home}>
         <Avatar
           alt='William Gillette'
-          src='https://avatars.githubusercontent.com/u/71407898?v=4'
+          src={headshot}
           sx={{ width: 250, height: 250, margin: '0 auto', marginBottom: '3vh' }}
         />
         <Typography className={styles.HomeHeader} sx={{fontFamily: 'Roboto Slab', margin: '0 auto', marginBottom: '3vh', textAlign:'center', fontSize: '2.5vh'}}>Triple Major in Mathematics, Computer Science, and Statistics, Student Athlete, Academic Chair of Phi Kappa Sigma, Lead Tutor in Math/CS/Statistics</Typography>
