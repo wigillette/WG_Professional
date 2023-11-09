5d6287bf6bd9f20a66675a46d3812c8c8635b50e
Will Gillette <wgillette02@gmail.com>
2023-11-09 14:40:34 -0500

    Updating

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -4,11 +4,11 @@ export default async function handler(req, res) {
     const { email, message } = req.body;
 
     if (email && message && message.length > 0 && email.length > 0) {
-        res.statusCode=200;
         res.setHeader('Content-Type', 'application/json');
         res.json({response: 'Successfully sent message!'});
+        res.status(200).end();
+    } else {
+        res.json({response: 'Failed to send message.'});
+        res.status(405).end();
     }
-
-    res.json({response: 'Failed to send message.'});
-    res.status(405).end();
 }
