2b73b80578522b1044c3e8901bb97ab0dd77f7c2
Will Gillette <wgillette02@gmail.com>
2023-11-11 18:57:11 -0500

    Setting up nodemailer

--- pages/api/message.js
+++ pages/api/message.js
@@ -1,3 +1,6 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
 const handler = (req, res) => {
     switch (req.method) {
         case 'POST':
@@ -11,16 +14,36 @@ const handler = (req, res) => {
 const sendMessage = (req, res) => {
     const { email, message } = req.body;
     res.setHeader('Content-Type', 'application/json');
-    let response = 'Failed to send message.';
-    let statusCode = 405;
     if (email && message && message.length > 0 && email.length > 0) {
         // Send message to email? Or update a database?
+        // Set up Nodemailer transport
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
 
-        response = 'Successfully sent message!';
-        statusCode = 200;
+        // Email options
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: 'wgillette02@gmail.com',
+            subject: `William Gillette Professional | Message from ${email}`,
+            html: `Dear Will,<br/>${message}<br/>From, ${email}`,
+        };
+
+        // Send the email
+        transporter.sendMail(mailOptions, (error) => {
+            res.json({response: error.message || 'Successfully sent message!'});
+            res.status(200).end();
+            console.log(error);
+        });
+    } else {
+        res.json({response: 'Failed to send message.'});
+        res.status(405).end();
     }
-    res.json({response: response});
-    res.status(statusCode).end();
+    
 }
 
 export default handler;
